8819416506ea775b8cfddf98a3583cc8f8f886a2
liChengYang <1945729758@qq.com>
2023-12-24 10:58:41 +0800

    MongoDB数据库环境搭建 以及C++调用数据库

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # OPOJ
+
 基于C++和Vue.js的OJ在线判题系统
 
+## 测试
 
-# 测试
   1.进入项目: cd ./.../OPOJ
   2.创建 build 文件夹并进入: mkdir build && cd build
   3.构建项目并编译: cmake && make
   4.运行: ./OPOJ
   
-  
-# 测试get请求
+## 测试get请求
+
   curl -i "127.0.0.1:8081/test/get?userid=1"
   
+## 测试post请求
 
-# 测试post请求
   curl -i -X POST -d '{"name":"lcy", "hobby":["running","reading","rap"]}' 127.0.0.1:8081/test/post
-  - i 输出 HTTP 头和返回内容。
-  - X 指定与服务器通信使用的请求方法
-  - d 主要是针对 http 协议的 post 请求指定要在消息体中发送的数据
+
+- i 输出 HTTP 头和返回内容。
+- X 指定与服务器通信使用的请求方法
+- d 主要是针对 http 协议的 post 请求指定要在消息体中发送的数据
+
+## MongoDB数据库环境搭建 以及C++调用数据库
+
+使用图形化界面：VSCode 左侧栏中 MongoDB 插件
+MongoDB学习：https://docs.mongoing.com/
--- main.cpp
+++ main.cpp
@@ -1,80 +1,72 @@
-#include "./include/httplib.h"           // HTTP库
-#include <jsoncpp/json/json.h> // JSON库
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/pipeline.hpp>
+
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+#include <mongocxx/pool.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+
 #include <iostream>
-#include <string>
+#include <jsoncpp/json/json.h>
+
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
 
 using namespace std;
-
-// get请求测试函数
-void doTestGet(const httplib::Request &req, httplib::Response &res)
-{
-    cout << "get请求测试" << endl;
-    // 获取请求头参数
-    string userid = req.get_param_value("userid");
-
-    // 处理逻辑......
-
-    // 返回数据
-    Json::Value resjson;
-    if (userid == "1")
-    {
-        resjson["userid"] = userid;
-        resjson["name"] = "lcy";
-        resjson["age"] = "22";
-        resjson["hobby"].append("basketball");
-        resjson["hobby"].append("reading");
-    }
-    else if (userid == "2")
-    {
-        resjson["userid"] = userid;
-        resjson["name"] = "LCY";
-        resjson["age"] = "18";
-        resjson["hobby"].append("travelling");
-        resjson["hobby"].append("running");
-    }
-    else
-    {
-        res.status = 404;
-    }
-
-    res.set_content(resjson.toStyledString(), "json");
-}
-
-// post请求测试函数
-void doTestPost(const httplib::Request &req, httplib::Response &res)
-{
-    cout << "post请求测试" << endl;
-    // 获取请求体
-    Json::Value jsonvalue;
-    Json::Reader reader;
-    // 解析传入的json
-    reader.parse(req.body, jsonvalue);
-
-    cout << "获取请求体信息：" << endl;
-    cout << jsonvalue.toStyledString() << endl;
-
-    // 处理逻辑......
-
-    Json::Value resjson;
-    resjson["result"] = "test post : process successfully";
-    resjson["info"] = jsonvalue;
-
-    res.set_content(resjson.toStyledString(), "json");
-}
+mongocxx::instance instance{};                                                                       // This should be done only once.
+mongocxx::uri uri{}; // 连接配置
+mongocxx::client client(uri);
 
 int main()
 {
-  using namespace httplib;
-  Server server;
+    Json::Value jsonvalue;
+    Json::Reader reader;
+    mongocxx::pipeline pipe;
+    bsoncxx::builder::stream::document document{};
+    auto collection = client["Mongotest"]["Grades"];
 
-  // get请求测试
-  server.Get("/test/get", doTestGet);
+    // 1. 查询全部
+    // mongocxx::cursor cursor = collection.find({});
+    // for (auto doc : cursor)
+    // {
+    //     reader.parse(bsoncxx::to_json(doc), jsonvalue);
+    //     cout << jsonvalue.toStyledString() << endl;
+    // }
 
-  // post请求测试
-  server.Post("/test/post", doTestPost);
+    // 2.查询年龄最小的两个人
+    pipe.sort({make_document(kvp("age", 1))});
+    pipe.limit(2);
 
-  // 设置监听端口
-  server.listen("0.0.0.0", 8081);
+    mongocxx::cursor cursor = collection.aggregate(pipe);
+    for (auto doc : cursor)
+    {
+        reader.parse(bsoncxx::to_json(doc), jsonvalue);
+        cout << jsonvalue.toStyledString() << endl;
+    }
 
-  return 0;
+    // 3.修改aaa的age为100
+    // document
+    //     << "$set" << open_document
+    //     << "age" << 100
+    //     << close_document;
+
+    // collection.update_one({make_document(kvp("name", "aaa"))},document.view());
+
+
+    // 4.删除ccc
+    collection.delete_one({make_document(kvp("name", "ccc"))});
+
+    return 0;
 }
